e5d1329951b11c12ee89580494f1e043dab873d9
NdouJ <79972144+NdouJ@users.noreply.github.com>
2024-01-20 23:20:40 +0100

    init

---
+++ src/main/java/hr/algebra/pawprotectorfront/PawProtectorFrontApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.pawprotectorfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawProtectorFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawProtectorFrontApplication.class, args);
+    }
+
+}
